3d3607744dedea6f6f2091662f9a34437a71259b
Roberto Polli <robipolli@gmail.com>
2024-07-01 11:25:21 +0200

    add snippet file.

--- tests/test_ctypes_snippet.py
+++ tests/test_ctypes_snippet.py
@@ -25,6 +25,44 @@ def test_parse_person():
     assert p.name == b"John Dow"
 
 
+#
+# $ ulimit -c unlimited
+# $ pytest -k null_safe
+# Segmentation fault (core dumped)
+#
 def test_is_null_safe():
     ret = le.parse_person(None, None)
     assert ret == -1
+
+
+from ctypes import *
+from ctypes import POINTER, Structure, c_int
+from pathlib import Path
+
+# Load libraries.
+cJSON_so = Path(__file__).parent / "libcjson.so"
+lc = CDLL(cJSON_so)
+
+
+#
+# Define cJSON struct in two steps to avoid circular references.
+#
+class cJSON(Structure):
+    pass
+
+
+cJSON._fields_ = [
+    ("next", POINTER(cJSON)),
+    ("prev", POINTER(cJSON)),
+    ("child", POINTER(cJSON)),
+    ("type", c_int),
+    ("valuestring", c_char_p),
+    ("valueint", c_int),
+    ("valuedouble", c_double),
+    ("string", c_char_p),
+]
+#
+# Associate a return type with cJSON_Parse.
+#
+cJSON_Parse = lc.cJSON_Parse
+cJSON_Parse.restype = POINTER(cJSON)
